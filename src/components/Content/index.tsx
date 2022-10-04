658a3f9b8bcdad0064b2adf1cc4a624140fe8a69
Gustavo <avoguga@gmail.com>
2022-10-04 14:36:23 -0300

    Adiciona funcionalidades de drag etc etc

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -3,15 +3,86 @@ import {
   FirstSection,
   SecondSection,
   ThirdSection,
+  ImgContainer,
+  UploadedImg,
+  DraggableBlock,
+  DraggableImg,
 } from "./styles";
+import Draggable from "react-draggable";
+import axios from "axios";
+import { useState } from "react";
+import PesoImg from "../../assets/peso.png";
+import PesoImg2 from "../../assets/pesoo.png";
 
 export function Content() {
+  // Hooks FirstSection
+  const [selectedFile, setSelectedFile]: any = useState();
+  const [image, setImage] = useState();
+  const [loading, setLoading] = useState(false);
+
+  // Functions and Utils FirstSection
+  const apiUrl = "http://localhost:8000/upload/arquivo";
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    try {
+      setLoading(true);
+      const response = await axios({
+        method: "post",
+        url: apiUrl,
+        data: formData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }).then((res) => {
+        setLoading(false);
+        setImage(res.data.url);
+      });
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  const handleFileSelect = (event: any) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   return (
     <MainContainer>
       <FirstSection>
-      
-
+        <form onSubmit={handleSubmit}>
+          <input type="file" onChange={handleFileSelect} />
+          <input type="submit" value="Upload File" />
+        </form>
+        <div style={{ display: "flex" }}>
+          <ImgContainer>
+            {loading == true ? (
+              <h3>Carregando...</h3>
+            ) : (
+              <UploadedImg src={image} alt="Imagem carregada" />
+            )}
+          </ImgContainer>
+          <div style={{display: "flex", justifyContent: "space-around"}}>
+            <Draggable handle="aaa">
+              <DraggableBlock id="aaa">
+                <DraggableImg src={PesoImg} />
+              </DraggableBlock>
+            </Draggable>
+            <Draggable>
+              <DraggableBlock>
+                <DraggableImg src={PesoImg2} />
+              </DraggableBlock>
+            </Draggable>
+          </div>
+        </div>
       </FirstSection>
+
+      <SecondSection></SecondSection>
     </MainContainer>
   );
 }
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -24,3 +24,29 @@ export const ThirdSection = styled.div`
   height: 100%;
   width: 100%;
 `;
+
+export const ImgContainer = styled.div`
+  width: auto;
+  height: auto;
+`;
+
+export const UploadedImg = styled.img`
+  width: 1000px;
+  height: 100%;
+`;
+
+export const DraggableImg = styled.img`
+  width: 100px;
+  height: 50px;
+  margin-top: 5px;
+`;
+
+export const DraggableBlock = styled.div`
+  height: 5px;
+  width: 10px;
+  border-radius: 200px;
+  background-color: red;
+  padding-top: 10px;
+`;
+
+
